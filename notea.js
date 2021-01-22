4e78fdacd9a5ef0948409e8c24db1cb570cd4e95
Nick Graffis <nicholasgraffis@gmail.com>
2021-01-22 05:29:07 -0800

    local storage wrapper

---
+++ notea.js
@@ -0,0 +1,14 @@
+//Update
+  //All
+  //All where param
+  //All with mult param
+localdb.update('dinosaur', {  })
+
+//Delete
+  //One with key
+  //All
+  //All where param
+
+//Drop
+  //All
+  //One Table
